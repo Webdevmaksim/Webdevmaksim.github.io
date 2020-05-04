2adc6feea7889e5f44bd127af9ad92798d050a68
unknown <maksim.saraev.121.5@gmail.com>
2020-05-04 23:41:20 +0300

    Added css-min inti the project, created css folder and css-min folder for minified css

--- gulpfile.js
+++ gulpfile.js
@@ -15,3 +15,15 @@ gulp.task('browser-sync', function() {
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+let cleanCSS = require('gulp-clean-css');
+// Task to minify css using package cleanCSs
+gulp.task('minify-css', () => {
+     // Folder with files to minify
+     return gulp.src('./css/*.css')
+     //The method pipe() allow you to chain multiple tasks together 
+     //I execute the task to minify the files
+    .pipe(cleanCSS())
+    //I define the destination of the minified files with the method dest
+    .pipe(gulp.dest('css-min'));
+});
