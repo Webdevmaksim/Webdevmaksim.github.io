a5d15294f413fc581081c150b1db711828c1fd35
unknown <maksim.saraev.121.5@gmail.com>
2020-05-17 12:54:44 +0300

    witched js to Jquery

--- js/main.js
+++ js/main.js
@@ -1,14 +1,42 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-    const closeBtn = document.querySelector('.modal__close');
-    const switchModal = () =>{
-        modal.classList.toggle('modal--visible');
-    }
+// document.addEventListener("DOMContentLoaded", function(event) { 
+//     const modal = document.querySelector('.modal');
+//     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//     const closeBtn = document.querySelector('.modal__close');
+//     const switchModal = () =>{
+//         modal.classList.toggle('modal--visible');
+//     }
 
-    modalBtn.forEach(element => {
-        element.addEventListener('click', switchModal);
+//     modalBtn.forEach(element => {
+//         element.addEventListener('click', switchModal);
+//     });
+
+//     closeBtn.addEventListener('click', switchModal);
+// });
+
+// Закрытие на esc и куда угодно но не на модальное окно 
+
+// const alternativeClose = (esc, fadeClick) => {
+//     document.addEventListener('click', function(event){
+
+//     });
+// }
+
+// document.addEventListener('keypress', (event) => {
+//     if (event.code == 'Esc'){
+//         console.log('event'),
+//         event.classList.toggle('modal--visible');
+//     }
+// });
+
+$(document).ready(function () {
+    let modal = $('.modal'),
+        modalBtn = $('[data-toggle="modal"]'),
+        closeBtn = $('.modal__close');
+
+    modalBtn.on('click', function(){
+       modal.toggleClass('modal--visible');
     });
-
-    closeBtn.addEventListener('click', switchModal);
+    closeBtn.on('click', function(){
+        modal.toggleClass('modal--visible');
+    });
 });
---
+++ js/test.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var modal = ('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closeBtn = $('.modal__close');
+    console.log(modal);
+    console.log(modalBtn);
+    console.log(closeBtn);
+});
