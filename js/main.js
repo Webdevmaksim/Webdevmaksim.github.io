2e29a83b0fbb8bbd34f5d823b8ceb6fe92da9872
unknown <maksim.saraev.121.5@gmail.com>
2020-05-18 18:16:23 +0300

    fixed breakpoin bug + made link repair prject

--- js/main.js
+++ js/main.js
@@ -31,19 +31,18 @@
 
 // JQuery Модальное окно
 $(document).ready(function () {
-    let modal = $('.modal'),
+    var modal = $('.modal'),
         modalBtn = $('[data-toggle="modal"]'),
         closeBtn = $('.modal__close');
+        modalD = $('.modal__dialog');
 
     modalBtn.on('click', function(){
        modal.toggleClass('modal--visible');
     });
     closeBtn.on('click', function(){
         modal.toggleClass('modal--visible');
-        if (event.target == modal) {
-            modal.toggleClass('modal--visible');
-        }
     });
+
 
      //initialize swiper when document ready
         var mySwiper = new Swiper ('.swiper-container', {
@@ -52,21 +51,37 @@ $(document).ready(function () {
         pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
-        spaceBetween: '100px',
       },
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-  })
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        }
+      },
+  });
         var next = $('.swiper-button-next');
         var prev = $('.swiper-button-prev');
         var bullets = $('.swiper-pagination');
         var bullet = $('.swiper-pagination-bullet');
         // Margin-positioning for the left arrow 
-        next.css('left', prev.width() + 10 + bullets.width() +20);
+        next.css('left', prev.width() + 10 + bullets.width() +40);
         bullets.css('left', prev.width() + 20);
-        bullet.css('right',  bullet.width() + 200);
+        // bullet.css('right',  bullet.width() + 30);
 
 });
 
