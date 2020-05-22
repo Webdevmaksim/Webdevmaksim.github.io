70ae35465902246afbc4339df36a3ca02fb9eb09
unknown <maksim.saraev.121.5@gmail.com>
2020-05-22 15:15:56 +0300

    finished Y-maps+adv:Y-maps

--- js/main.js
+++ js/main.js
@@ -177,6 +177,40 @@ $(document).ready(function () {
         });
         // маска для телефона
           $('[type=tel]').mask('+7 (000) 000-00-00');
+        // Яндекс Карты
+          ymaps.ready(function () {
+            var myMap = new ymaps.Map('map', {
+                    center: [53.919200, 27.494623],
+                    zoom: 12
+                }, {
+                    searchControlProvider: 'yandex#search'
+                }),
+        
+                // Создаём макет содержимого.
+                MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+                    '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+                ),
+        
+                myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                    hintContent: 'Glo-Academy:Repair-Design',
+                    balloonContent: 'Метка для дз номер 25'
+                }, {
+                    // Опции.
+                    // Необходимо указать данный тип макета.
+                    iconLayout: 'default#image',
+                    // Своё изображение иконки метки.
+                    iconImageHref: '../img/renovation.png',
+                    // Размеры метки.
+                    iconImageSize: [32, 32],
+                    // Смещение левого верхнего угла иконки относительно
+                    // её "ножки" (точки привязки).
+                    iconImageOffset: [-5, -38]
+                });        
+                
+        
+            myMap.geoObjects
+                .add(myPlacemark);
+        });
 });
 
 
